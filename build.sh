307cd91de2f4f91b7e51e49ad39d8f6f070de38f
Rosalie Wagner <mail@rosaliewagner.com>
2021-10-21 10:21:01 +0200

    Added missing components and regenerated fonts

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e 
 
+# Requires python3 and gftools (pip install gftools)
+
 gftools builder Block/sources/config.yaml
 gftools builder Circular/sources/config.yaml
 gftools builder Rounded/sources/config.yaml
+
